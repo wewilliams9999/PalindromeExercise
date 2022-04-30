a4b62028891907328632772dc6521cd0455c3da4
wewilliams9999 <williamswallise@gmail.com>
2022-04-30 15:49:55 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome (string word)
+        {
+            //see if word is a palindrome
+            //is reverse of word the same as word
+            //how do i reverse a word?
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            var reversed = new string (charArray);
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
+    {   
+        //testing whether my IsAPalindromeMethod works correctly
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void TestPalindrome(string word, bool expected)
         {
+            //Arrange
+            var wordSmith = new WordSmith();
 
+            //Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
